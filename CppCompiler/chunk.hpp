a8b54c64ac76ce0eededbf10d613185c450cf468
SamarthGupta23 <125825832+SamarthGupta23@users.noreply.github.com>
2025-06-26 23:18:09 +0530

    Update chunk.hpp

--- CppCompiler/chunk.hpp
+++ CppCompiler/chunk.hpp
@@ -1,17 +1,22 @@
 #pragma once
 #include "common.hpp"
-enum class Opcode: uint8_t {
-	OP_RETURN,
+#include "value.hpp"
+
+enum class Opcode : int {
+    OP_RETURN,    //1 byte OPCODE
+    OP_CONSTANT, //2 byte OPCODE
 };
 
 class Chunk {
-	std::vector <Opcode> code; //vector of bytecode
-
 public:
-	void initChunk();
-	void writeChunk(Opcode byte);
-	void freeChunk();
-
-	//getters
-	std::vector <Opcode> getCode();
+    std::vector <Opcode> code; //vector of bytecode
+    std::vector <int> lines; //we store lines in this , line number of where the opcode was passed into it
+	valueArray constants; //vector of constants
+    
+    void initChunk();
+    void writeChunk(Opcode byte , int line);
+    void freeChunk();
+    int addConstant(Value value);  // FIX: Return int, not Opcode
+    //getters
+    std::vector <Opcode> getCode();
 };
